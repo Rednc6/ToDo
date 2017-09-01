15e492cc77c5921e2dcd6b6cb0b993f24e4dae7e
Rednc6 <mich475r@edu.easj.dk>
2017-09-01 12:41:48 +0200

    Added 2 models, 1) activities, 2) activityfunctions. Folder set for mvvm structure.

---
+++ ToDo/Model/Activities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Model
+{
+    class Activities
+    {
+        public DateTime Date { get; set; }
+        public String Location { get; set; }
+        public String Information { get; set; }
+
+        public Activities(DateTime date, String location, String Information)
+        {
+            this.Date = date;
+            this.Location = location;
+            this.Information = Information;
+        }
+    }
+}
---
+++ ToDo/Model/ActivityFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Model
+{
+    class ActivityFunctions
+    {
+        public List<Activities> ActivityList { get; set; }
+
+        public ActivityFunctions()
+        {
+            ActivityList = new List<Activities>();
+        }
+
+        public void AddToList()
+        {
+            
+        }
+
+        public void RemoveFromList()
+        {
+            
+        }
+    }
+}
